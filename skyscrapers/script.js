2960ab20996b2616077732cdca649116f1507bde
NSI-Projects <nsi.projects.contact@gmail.com>
2025-12-14 19:54:13 +0100

    Update script.js

--- skyscrapers/script.js
+++ skyscrapers/script.js
@@ -13,3 +13,17 @@ document.getElementById("generate").addEventListener("click", () => {
         btn.disabled = false;
     }, 2000);
 });
+
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+document.addEventListener("keydown", (e) => {
+    if (
+        (e.ctrlKey && e.key.toLowerCase() === "u") ||
+        (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") ||
+        (e.key === "F12") ||
+        (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "c") ||
+        (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "j")
+    ) {
+        e.preventDefault();
+    }
+});
